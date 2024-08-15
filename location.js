fa9bd1593d31784a6e98cf3eef8e1297adb7dc14
anandu20 <ananduramachandran411@gmail.com>
2024-08-15 09:23:12 +0530

    party

--- location.js
+++ location.js
@@ -1,10 +1,8 @@
 async function search(){
     try {
-        
         const a=document.getElementById("txt").value
         const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${a}&appid=d82dce617113dc86ab2408e76df76adb`)
-        console.log(res);
-        
+        console.log(res);     
         if(res.status==404){
             console.log("Location not found");
         }
@@ -13,22 +11,20 @@ async function search(){
             console.log("weather update");
             document.getElementById("print").innerHTML=`
               <div class="print1" id="print1">
-            <h2 class="m1">${weather.name}</h2>
+            <h1 class="m1">${weather.name}</h1>
             <h2 class="m2">${(weather.main.temp-263.165).toString().substring(0,4)} <sup>°C</sup></h2>
-             <img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="icon">
-             
-
-   
-
+            <h4 class="m3">${weather.weather[0].main}</h4>
+            <h2 id="h">Weather Details</h2><img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="icon">
+            
             <table >
-                <tr>
+                <tr>              
                     <td >
                         <p>Feels like</p>
                         <h3>${weather.main.feels_like}</h3> 
                     </td>
                     <td>
                         <p>Condition</p>
-                         <h3 class="m3">${weather.weather[0].main}</h3>
+                         <h3 class="m4">${weather.weather[0].main}</h3>
                     </td>
                 </tr>  
                 <tr>
@@ -38,7 +34,7 @@ async function search(){
                     </td>
                     <td>
                         <p>Humdity</p>
-                        <h3>${weather.main.humidity}</h3>
+                        <h3>${weather.main.humidity}°C</h3>
                     </td>
 
                 </tr>
@@ -51,21 +47,11 @@ async function search(){
                         <p>Air pressure</p>
                         <h3>${weather.main.pressure}</h3>
                     </td>
-
                 </tr>
-
-
             </table>
-
-
-            
-
         </div>`
         }
-    } catch (error) {
-        
-        console.log("error");
-        
-    }
-    
+    } catch (error) {       
+        console.log("error"); 
+    } 
 }
